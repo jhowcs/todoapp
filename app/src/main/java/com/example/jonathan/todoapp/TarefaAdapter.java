9ee7aa14353eb8b3cbca252bc81e67b2281595d5
Jonathan Campos <jonathan@Jonathans-MacBook-Pro.local>
2018-03-13 22:23:42 -0300

    add method to strike text for finished tasks

--- app/src/main/java/com/example/jonathan/todoapp/TarefaAdapter.java
+++ app/src/main/java/com/example/jonathan/todoapp/TarefaAdapter.java
@@ -1,11 +1,13 @@
 package com.example.jonathan.todoapp;
 
+import android.graphics.Paint;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -32,8 +34,25 @@ public class TarefaAdapter
     public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
         TarefaModelo tarefaModelo = lista.get(position);
         TarefaViewHolder vh = (TarefaViewHolder) holder;
+
+        addChangeListenerForCheckBox(vh.chkExecutado, vh.txtDescricao);
+
         vh.txtDescricao.setText(tarefaModelo.getDescricao());
         vh.chkExecutado.setChecked(tarefaModelo.isExecutado());
+
+    }
+
+    private void addChangeListenerForCheckBox(final CheckBox chkExecutado, final TextView txtDescricao) {
+        chkExecutado.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                final int paintFlags = isChecked
+                        ? txtDescricao.getPaintFlags() | Paint.STRIKE_THRU_TEXT_FLAG
+                        : Paint.LINEAR_TEXT_FLAG;
+
+                    txtDescricao.setPaintFlags(paintFlags);
+            }
+        });
     }
 
     @Override
