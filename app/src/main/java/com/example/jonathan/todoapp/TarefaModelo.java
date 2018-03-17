686d40ebc3e5845e947f8d73eb741f24ca8d334a
Jonathan Campos <jonathan@Jonathans-MacBook-Pro.local>
2018-03-17 11:45:36 -0300

    Add annotations that will be used by Room to persist data

--- app/src/main/java/com/example/jonathan/todoapp/TarefaModelo.java
+++ app/src/main/java/com/example/jonathan/todoapp/TarefaModelo.java
@@ -1,12 +1,28 @@
 package com.example.jonathan.todoapp;
 
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.PrimaryKey;
+
+@Entity(tableName = "task")
 public class TarefaModelo {
+
+    @PrimaryKey(autoGenerate = true)
+    private long uid;
+
     private String descricao;
     private boolean isExecutado;
 
     public TarefaModelo(String descricao, boolean isExecutado) {
         this.descricao = descricao;
         this.isExecutado = isExecutado;
+    }
+
+    public long getUid() {
+        return uid;
+    }
+
+    public void setUid(long uid) {
+        this.uid = uid;
     }
 
     public String getDescricao() {
