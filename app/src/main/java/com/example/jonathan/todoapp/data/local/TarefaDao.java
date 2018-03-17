2864fef9fdaa9016ac844f8b2d328cd4a22018ad
Jonathan Campos <jonathan@Jonathans-MacBook-Pro.local>
2018-03-17 01:01:31 -0300

    Add update method in taskDao

--- app/src/main/java/com/example/jonathan/todoapp/data/local/TarefaDao.java
+++ app/src/main/java/com/example/jonathan/todoapp/data/local/TarefaDao.java
@@ -1,9 +1,12 @@
-package com.example.jonathan.todoapp;
+package com.example.jonathan.todoapp.data.local;
 
 import android.arch.persistence.room.Dao;
 import android.arch.persistence.room.Delete;
 import android.arch.persistence.room.Insert;
 import android.arch.persistence.room.Query;
+import android.arch.persistence.room.Update;
+
+import com.example.jonathan.todoapp.data.TarefaModelo;
 
 import java.util.List;
 
@@ -21,4 +24,7 @@ public interface TarefaDao {
 
     @Delete
     void delete(TarefaModelo tarefaModelo);
+
+    @Update
+    void update(TarefaModelo tarefaModelo);
 }
