8ea246c861bc4e79eaee0773d8c374735aa7e5df
jonathan <jhowcs@hotmail.com>
2018-03-17 23:05:22 -0300

    Add a setUp and a new test method
    - add setUp to switch to in memory database
    - add a new test to validate item lista count
    - add method to populate database with static data

--- app/src/androidTest/java/com/example/jonathan/todoapp/MainActivityTest.java
+++ app/src/androidTest/java/com/example/jonathan/todoapp/MainActivityTest.java
@@ -1,6 +1,7 @@
 package com.example.jonathan.todoapp;
 
 import android.content.Intent;
+import android.support.test.InstrumentationRegistry;
 import android.support.test.espresso.Espresso;
 import android.support.test.espresso.action.ViewActions;
 import android.support.test.espresso.intent.Intents;
@@ -9,6 +10,10 @@ import android.support.test.espresso.intent.rule.IntentsTestRule;
 import android.support.test.espresso.matcher.ViewMatchers;
 import android.support.test.runner.AndroidJUnit4;
 
+import com.example.jonathan.todoapp.data.DatabaseConcrete;
+import com.example.jonathan.todoapp.data.TarefaDao;
+
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -23,9 +28,32 @@ public class MainActivityTest {
                     true,
                     false);
 
+    @Before
+    public void setUp() {
+        DatabaseConcrete.switchToInMemoryDatabase(InstrumentationRegistry.getTargetContext());
+        popularBancoComDadosEstaticos();
+    }
+
+    private void popularBancoComDadosEstaticos() {
+        TarefaDao dao = DatabaseConcrete
+                .getInstance(InstrumentationRegistry.getTargetContext())
+                .getTarefaDao();
+
+        dao.insertAll(new TarefaModelo("Tarefa 1", false),
+                new TarefaModelo("Tarefa 2", false),
+                new TarefaModelo("Tarefa 3", false),
+                new TarefaModelo("Tarefa 4", false),
+                new TarefaModelo("Tarefa 5", false),
+                new TarefaModelo("Tarefa 6", false));
+    }
+
+    private void iniciarActivity() {
+        rule.launchActivity(new Intent());
+    }
+
     @Test
     public void aoClicarNoBotaoDeIncluir_deveAbrirUIDeInclusaoDeTarefa() throws Exception {
-        rule.launchActivity(new Intent());
+        iniciarActivity();
 
         Espresso.onView(ViewMatchers.withId(R.id.fabNovaTarefa))
                 .perform(ViewActions.click());
@@ -34,4 +62,10 @@ public class MainActivityTest {
                 IntentMatchers.hasComponent(NovaTarefaActivity.class.getName()));
     }
 
+    @Test
+    public void aoIniciaActivity_deveExibirListaComSeisTarefas() {
+        iniciarActivity();
+        Espresso.onView(ViewMatchers.withId(R.id.rvListaTarefa))
+                .check(RecyclerViewItemCountAssertion.withItemCount(6));
+    }
 }
