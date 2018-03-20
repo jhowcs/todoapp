975fe7054ba6c0bc25e521dce024136dd84ee816
jonathan <jhowcs@hotmail.com>
2018-03-19 22:47:01 -0300

    Add delete method

--- app/src/main/java/com/example/jonathan/todoapp/data/TarefaDao.java
+++ app/src/main/java/com/example/jonathan/todoapp/data/TarefaDao.java
@@ -1,6 +1,7 @@
 package com.example.jonathan.todoapp.data;
 
 import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Delete;
 import android.arch.persistence.room.Insert;
 import android.arch.persistence.room.Query;
 import android.arch.persistence.room.Update;
@@ -20,4 +21,7 @@ public interface TarefaDao {
 
     @Update
     int update(TarefaModelo tarefa);
+
+    @Delete
+    void delete(TarefaModelo tarefa);
 }
