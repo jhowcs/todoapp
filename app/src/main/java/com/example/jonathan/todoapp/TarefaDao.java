e8d41964c8f9ea45ae9f91fe0d7fd083ca761048
jonathan <jhowcs@hotmail.com>
2018-03-24 13:38:40 -0300

    Add Room contract classes

---
+++ app/src/main/java/com/example/jonathan/todoapp/TarefaDao.java
@@ -0,0 +1,25 @@
+package com.example.jonathan.todoapp;
+
+import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Delete;
+import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.Query;
+import android.arch.persistence.room.Update;
+
+import java.util.List;
+
+@Dao
+public interface TarefaDao {
+
+    @Query("select * from tarefa")
+    List<TarefaModelo> getListaDeTarefas();
+
+    @Insert
+    long inserirTarefa(TarefaModelo tarefa);
+
+    @Update
+    int atualizarTarefa(TarefaModelo tarefa);
+
+    @Delete
+    int excluirTarefa(TarefaModelo tarefaParaExclusao);
+}
--- app/src/main/java/com/example/jonathan/todoapp/TarefaModelo.java
+++ app/src/main/java/com/example/jonathan/todoapp/TarefaModelo.java
@@ -1,6 +1,15 @@
 package com.example.jonathan.todoapp;
 
-public class TarefaModelo {
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.PrimaryKey;
+import android.os.Parcel;
+import android.os.Parcelable;
+
+@Entity(tableName = "tarefa")
+public class TarefaModelo implements Parcelable {
+
+    @PrimaryKey(autoGenerate = true)
+    private long id;
     private String descricao;
     private boolean isExecutado;
 
@@ -8,6 +17,23 @@ public class TarefaModelo {
         this.descricao = descricao;
         this.isExecutado = isExecutado;
     }
+
+    protected TarefaModelo(Parcel in) {
+        descricao = in.readString();
+        isExecutado = in.readByte() != 0;
+    }
+
+    public static final Creator<TarefaModelo> CREATOR = new Creator<TarefaModelo>() {
+        @Override
+        public TarefaModelo createFromParcel(Parcel in) {
+            return new TarefaModelo(in);
+        }
+
+        @Override
+        public TarefaModelo[] newArray(int size) {
+            return new TarefaModelo[size];
+        }
+    };
 
     public String getDescricao() {
         return descricao;
@@ -23,5 +49,24 @@ public class TarefaModelo {
 
     public boolean isExecutado() {
         return isExecutado;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(descricao);
+        dest.writeByte((byte) (isExecutado ? 1 : 0));
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
     }
 }
