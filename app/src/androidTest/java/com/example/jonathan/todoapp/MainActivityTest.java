3b726cc80cb3ff15bdd78487221f00acdacb133f
jonathan <jhowcs@hotmail.com>
2018-03-24 14:30:11 -0300

    Add method to create and populate inMemoryDatabase

--- app/src/androidTest/java/com/example/jonathan/todoapp/MainActivityTest.java
+++ app/src/androidTest/java/com/example/jonathan/todoapp/MainActivityTest.java
@@ -1,6 +1,7 @@
 package com.example.jonathan.todoapp;
 
 import android.content.Intent;
+import android.support.test.InstrumentationRegistry;
 import android.support.test.espresso.Espresso;
 import android.support.test.espresso.action.ViewActions;
 import android.support.test.espresso.intent.Intents;
@@ -9,6 +10,8 @@ import android.support.test.espresso.intent.rule.IntentsTestRule;
 import android.support.test.espresso.matcher.ViewMatchers;
 import android.support.test.runner.AndroidJUnit4;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -23,8 +26,34 @@ public class MainActivityTest {
                     true,
                     false);
 
+    @Before
+    public void setUp() {
+        AppDatabase.swapToInMemomyDatabase(
+                InstrumentationRegistry.getTargetContext());
+        popularBancoDeDadosComFakeData();
+    }
+
+    private void popularBancoDeDadosComFakeData() {
+        AppDatabase db = AppDatabase
+                .appDatabaseInstance(
+                        InstrumentationRegistry.getTargetContext());
+        TarefaDao dao = db.getTarefaDao();
+
+        dao.inserirTarefa(new TarefaModelo("tarefa 1", false));
+        dao.inserirTarefa(new TarefaModelo("tarefa 2", false));
+        dao.inserirTarefa(new TarefaModelo("tarefa 3", false));
+        dao.inserirTarefa(new TarefaModelo("tarefa 4", false));
+        dao.inserirTarefa(new TarefaModelo("tarefa 5", false));
+
+    }
+
+    @After
+    public void tearDown() {
+
+    }
+
     @Test
-    public void aoClicarNoBotaoDeIncluir_deveAbrirUIDeInclusaoDeTarefa() throws Exception {
+    public void aoClicarNoBotaoDeIncluir_deveAbrirUIDeInclusaoDeTarefa() {
         rule.launchActivity(new Intent());
 
         Espresso.onView(ViewMatchers.withId(R.id.fabNovaTarefa))
@@ -32,6 +61,7 @@ public class MainActivityTest {
 
         Intents.intended(
                 IntentMatchers.hasComponent(NovaTarefaActivity.class.getName()));
+
     }
 
 }
